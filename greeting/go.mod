73ac81665d746779bdc6d19deefc5899af1a13fd
jaenmo <j.enjuto.monje@gmail.com>
2022-07-31 12:00:04 +0100

    modules

---
+++ greeting/go.mod
@@ -0,0 +1,3 @@
+module github.com/jaenmo/Hi/greeting
+
+go 1.18
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import ("github.com/jaenmo/Hi/greeting"
-		"github.com/jaenmo/Hi/greeting/Spanish")
+		"github.com/jaenmo/Hi/greeting/Spanish"
+		"github.com/sirupsen/logrus")
 
 func main(){
 	greeting.Hello()
 	greeting.Hi()
 	spanish.Hola()
 	spanish.Adios()
+	logrus.Println("Oh mininio")
 }
